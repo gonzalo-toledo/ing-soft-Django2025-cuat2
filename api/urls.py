782a8217ee3c8e037f548cbb1f100e5256a2a34f
Gonzalo Toledo <g.toledo@itecriocuarto.org.ar>
2025-08-31 17:10:26 -0300

    Añade serializadores y vistas para el registro de usuarios y pasajeros, incluyendo la creación de un nuevo usuario y pasajero en un solo endpoint.

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,34 @@
 from django.urls import path
-
 from api.views import (
     UserListCreateView,
     UserRetrieveUpdateDestroyView,
+    RegistroCreateView,
+    # PasajeroNuevoCreateView,
+    # PasajeroRetrieveUpdateDestroyView,
+    PasajeroListCreateView
 )
 
 urlpatterns = [
-    #USERS
+    # USERS
     path('users/', UserListCreateView.as_view(), name='users-list'),
     path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='users-detail'),
+
+    # PASAJEROS
+    path('users/<int:user_pk>/pasajeros/', PasajeroListCreateView.as_view(), name='pasajeros-list'),
+
+    # USER + PASAJERO
+    path('registro/', RegistroCreateView.as_view(), name='registro'),
+    
+
+
+#     # REGISTRO INICIAL (usuario + pasajero)
+#     path('registro/', RegistroCreateView.as_view(), name='registro'),
+
+#     # PASAJEROS ADICIONALES (usuario logueado)
+#     path('pasajeros/nuevo/', PasajeroNuevoCreateView.as_view(), name='pasajero-nuevo'),
+    
+#     path('users/<int:user_pk>/pasajeros/', PasajeroListCreateView.as_view(), name='pasajeros-list'),
+
+#     # DETALLE / ACTUALIZACION / ELIMINACION DE PASAJERO
+#     path('pasajeros/<int:pk>/', PasajeroRetrieveUpdateDestroyView.as_view(), name='pasajero-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,13 +1,21 @@
 from django.contrib.auth.models import User
+from pasajeros.models import Pasajero
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import (
     ListAPIView,
     ListCreateAPIView,
-    RetrieveUpdateDestroyAPIView
+    RetrieveUpdateDestroyAPIView,
+    CreateAPIView
 )
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from api.serializers import UserSerializer, PasajeroSerializer, RegistroSerializer
 
-from api.serializers import UserSerializer
+from pasajeros.models import Pasajero 
+
+
+# USERS
 
 # class UserListView(ListAPIView):
 #     '''
@@ -17,7 +25,8 @@ from api.serializers import UserSerializer
 #     queryset = User.objects.all()
 #     serializer_class = UserSerializer
     
-    
+
+
 class UserListCreateView(ListCreateAPIView):
     '''
     GET /api/users/
@@ -51,3 +60,103 @@ class UserRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
             {"detail": "Usuario desactivado correctamente."},
             status=status.HTTP_200_OK
         )
+        
+
+
+#PASAJEROS
+class PasajeroListCreateView(ListCreateAPIView):
+    """
+    GET /api/users/<user_pk>/pasajeros/ -> lista pasajeros del usuario
+    POST /api/users/<user_pk>/pasajeros/ -> crea pasajero para el usuario
+    """
+
+    serializer_class = PasajeroSerializer
+
+    # redefino el queryset para filtrar por usuario:
+    def get_queryset(self):
+        user_pk = self.kwargs['user_pk']
+        return Pasajero.objects.filter(usuario_id=user_pk)
+
+    def perform_create(self, serializer):
+        user_pk = self.kwargs['user_pk'] # obtengo el user_pk de la URL
+        usuario = get_object_or_404(User, pk=user_pk)
+        serializer.save(usuario=usuario)
+
+
+
+#USUARIO + PASAJERO
+class RegistroCreateView(CreateAPIView):
+    """
+    POST /api/registro/ -> crea un nuevo usuario + pasajero
+    """
+    queryset = Pasajero.objects.all()
+    serializer_class = RegistroSerializer
+
+
+
+
+
+
+
+# #REGISTRO INICIAL (USUARIO + PASAJERO)
+# class RegistroCreateView(ListCreateAPIView):
+#     """
+#     POST /api/registro/ -> crea un nuevo usuario + pasajero
+#     GET /api/registro/ -> lista pasajeros creados (opcional)
+#     """
+#     queryset = Pasajero.objects.all()
+#     serializer_class = RegistroSerializer
+
+
+# # -----------------------------------
+# # Pasajeros adicionales para usuario logueado
+# # -----------------------------------
+# class PasajeroNuevoCreateView(ListCreateAPIView):
+#     """
+#     POST /api/pasajeros/nuevo/ -> crea un pasajero adicional para el usuario logueado
+#     GET /api/pasajeros/nuevo/ -> lista los pasajeros del usuario logueado
+#     """
+#     serializer_class = PasajeroNuevoSerializer
+#     # permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         # Solo los pasajeros del usuario logueado
+#         return Pasajero.objects.filter(usuario=self.request.user)
+
+#     def perform_create(self, serializer):
+#         serializer.save(usuario=self.request.user)
+
+# class PasajeroListCreateView(ListCreateAPIView):
+#     """
+#     GET /api/users/<user_pk>/pasajeros/ -> lista pasajeros del usuario
+#     POST /api/users/<user_pk>/pasajeros/ -> crea pasajero para el usuario
+#     """
+#     serializer_class = PasajeroSerializer
+#     permission_classes = [IsAuthenticated]
+
+#     def get_queryset(self):
+#         user_pk = self.kwargs['user_pk']
+#         return Pasajero.objects.filter(usuario_id=user_pk)
+
+#     def perform_create(self, serializer):
+#         user_pk = self.kwargs['user_pk']
+#         usuario = get_object_or_404(User, pk=user_pk)
+#         serializer.save(usuario=usuario)
+
+
+# # -----------------------------------
+# # Detalle / Actualización / Eliminación de un pasajero
+# # -----------------------------------
+# class PasajeroRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+#     """
+#     GET /api/pasajeros/1/ -> detalle pasajero 1
+#     PUT /api/pasajeros/1/ -> actualiza pasajero 1
+#     PATCH /api/pasajeros/1/ -> actualiza parcialmente pasajero 1
+#     DELETE /api/pasajeros/1/ -> elimina pasajero 1
+#     """
+#     queryset = Pasajero.objects.all()
+#     serializer_class = PasajeroSerializer
+#     # permission_classes = [IsAuthenticated]
+
+#     def perform_destroy(self, instance):
+#         instance.delete()
