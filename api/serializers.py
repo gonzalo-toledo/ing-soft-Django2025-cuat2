2f999afe16d666806c9decc8366b1fb0f25b4480
Gonzalo Toledo <g.toledo@itecriocuarto.org.ar>
2025-08-30 23:58:54 -0300

    Implementa la creación y actualización de usuarios en el serializador, incluyendo manejo de contraseñas y desactivación de usuarios en la vista.

--- api/serializers.py
+++ api/serializers.py
@@ -19,3 +19,44 @@ class UserSerializer(serializers.ModelSerializer):
             'is_active',
             'password'
         ]
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password:
+            user.set_password(password)
+        else:
+            user.set_password(User.objects.make_random_password())
+            #enviarle la contraseña por email
+        user.save()
+        #! Ver: al momento de crear un usuario, crearlo tambien como pasajero. Tener en cuenta que el modelo de pasajero tiene mas campos que el de usuario.
+        #por ej en el proyecto anterior para crear un cliente podemos hacer:
+        #Customer.objects.create (
+        #   name=user.first_name,
+        #   email=user.email,
+        #   phone='123',
+        # )
+        return user
+    
+    
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        
+        #Opcion 1
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+        
+        #Opcion 2: #!ESTA MAL PORQUE CREA UN NUEVO USUARIO
+        # instance = User.objects.create(
+        #     username=validated_data['username'],
+        #     email=validated_data['email'],
+        #     first_name=validated_data['first_name'],
+        #     last_name=validated_data['last_name'],
+        # )
+        # instance.set_password(password)
+        # instance.save()
+        # return instance       
